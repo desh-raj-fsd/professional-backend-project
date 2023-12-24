65a45f8c1beb189e07833de50aa80909f0620531
desh-raj-fsd <deshraj.fullstackdev@gmail.com>
2023-12-24 22:01:22 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,10 @@
+`use strict`
+// require('dotenv').config({path:'./env'});
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./env'
+})
+
+connectDB();
